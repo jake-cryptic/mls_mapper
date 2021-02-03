43b86e3db04ec01ebf46d13f7d45d0758263250f
Jake Mcneill <jake-cryptic@users.noreply.github.com>
2021-02-03 17:01:24 +0000

    Updated O2 XYZ tiles, added LTE-M layer

--- mappr/assets/js/map.js
+++ mappr/assets/js/map.js
@@ -1,4 +1,5 @@
 const O2_TMS_BASE = "https://68aa7b45-tiles.spatialbuzz.net/tiles/o2_uk-";
+const O2_TMS_VER = 166;
 //const EE_TMS_BASE = "https://maps.ee.co.uk//geowebcache/service/gmaps?&zoom={z}&x={x}&y={y}&format=image/png&Layers=";
 const EE_TMS_BASE = "https://coverage.ee.co.uk/geowebcache/service/gmaps?&zoom={z}&x={x}&y={y}&format=image/png&Layers=";
 const THREE_TMS_BASE = "http://www.three.co.uk/static/images/functional_apps/coverage/";
@@ -149,13 +150,14 @@ let v = {
 				"CM-L23":CM_TMS_BASE + "10&RAT=LTE&z={z}&x={x}&y={y}&band=40",
 				"CM-L08":CM_TMS_BASE + "10&RAT=LTE&z={z}&x={x}&y={y}&band=20",
 				"CM-L09":CM_TMS_BASE + "10&RAT=LTE&z={z}&x={x}&y={y}&band=8",
-				"3g2100-1B":O2_TMS_BASE + "v162/styles/o2_uk_v162_data/{z}/{x}/{y}.png",
-				"3g2100":O2_TMS_BASE + "v163/styles/o2_uk_v163_data/{z}/{x}/{y}.png",
-				"3g":O2_TMS_BASE + "v163/styles/o2_uk_v163_datacombined/{z}/{x}/{y}.png",
-				"4g":O2_TMS_BASE + "v163/styles/o2_uk_v163_lte/{z}/{x}/{y}.png",
-				"5g-1B":O2_TMS_BASE + "v162/styles/o2_uk_v162_5g/{z}/{x}/{y}.png",
-				"5g":O2_TMS_BASE + "v163/styles/o2_uk_v163_5g/{z}/{x}/{y}.png",
-				"VoLTE":O2_TMS_BASE + "v163/styles/o2_uk_v163_volte/{z}/{x}/{y}.png"
+				"3g2100-1B":O2_TMS_BASE + "v" + (O2_TMS_VER - 1) + "/styles/o2_uk_v" + (O2_TMS_VER - 1) + "_data/{z}/{x}/{y}.png",
+				"3g2100":O2_TMS_BASE + "v" + O2_TMS_VER + "/styles/o2_uk_v" + O2_TMS_VER + "_data/{z}/{x}/{y}.png",
+				"3g":O2_TMS_BASE + "v" + O2_TMS_VER + "/styles/o2_uk_v" + O2_TMS_VER + "_datacombined/{z}/{x}/{y}.png",
+				"4g":O2_TMS_BASE + "v" + O2_TMS_VER + "/styles/o2_uk_v" + O2_TMS_VER + "_lte/{z}/{x}/{y}.png",
+				"LTE-M":O2_TMS_BASE + "v" + O2_TMS_VER + "/styles/o2_uk_v" + O2_TMS_VER + "_ltem/{z}/{x}/{y}.png",
+				"5g-1B":O2_TMS_BASE + "v" + (O2_TMS_VER - 1) + "/styles/o2_uk_v" + (O2_TMS_VER - 1) + "_5g/{z}/{x}/{y}.png",
+				"5g":O2_TMS_BASE + "v" + O2_TMS_VER + "/styles/o2_uk_v" + O2_TMS_VER + "_5g/{z}/{x}/{y}.png",
+				"VoLTE":O2_TMS_BASE + "v" + O2_TMS_VER + "/styles/o2_uk_v" + O2_TMS_VER + "_volte/{z}/{x}/{y}.png"
 			},
 			"Three-UK":{
 				"CM-4G":CM_TMS_BASE + "20&RAT=LTE&z={z}&x={x}&y={y}&band=0",
